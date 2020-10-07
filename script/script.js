47b0482bc200de0ca56b4803d4eabca0276136fc
jalaniz-sys <60115425+jalaniz-sys@users.noreply.github.com>
2020-10-07 13:51:49 -0500

    created validated functioon and refer to it in the header of <form>

---
+++ script/script.js
@@ -0,0 +1,13 @@
+function ValidatedForm()
+{
+	var buttonOptions = document.getElementsByName("duck");
+	for(var x =0; x<buttonOptions.length; x++)
+	{
+		if(buttonOptions[x].checked !=false)
+		{
+			if(confirm("You have chosen "+buttonOptions[x].value+". Do you wish to keep this?"))
+				return true;
+		}
+	}
+
+}
